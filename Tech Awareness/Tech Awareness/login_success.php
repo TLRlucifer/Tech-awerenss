28affddb99900aa4d26bd14b45833c67f08ad341
TLRlucifer <robobuilder22@gmail.com>
2014-10-11 10:02:37 -0400

    adding code

---
+++ Tech Awareness/Tech Awareness/login_success.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Login Succesful";
+//header("location:main_login.php");
+if($myusername = stevemac){
+    header("location:machineshop.php");
+}
+?>
+
+<html>
+<body>
+Login Successful
+</body>
+</html>
